250f5edb8257ffb4fff852856e938a5ae685840f
Afhaam1 <84372124+Afhaam1@users.noreply.github.com>
2022-04-10 14:25:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var noseX = ""
+var noseY = ""
+function preload() {
+    Clown = loadImage("https://i.postimg.cc/pV47bSPg/lips-removebg-preview.png")
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.size(300 ,300)
+    video.hide()
+
+    poseNet=ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialize')
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x
+        noseY = results[0].pose.nose.y
+    }
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(Clown, noseX -22, noseY, 40, 40);
+}
+function take_snapshot(){
+    save('Filter_Image.png');
+}
